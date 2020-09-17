2c391e0bbf89553791a89479ff6843bcaaf3591d
sergo1996 <56196384+sergo1996@users.noreply.github.com>
2020-09-17 22:09:56 +0300

    Updates

---
+++ precache-manifest.4edb8dc5287cd13a48dfcab30b10cc52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428c65542c1ef7a3772e6963247d2c48",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "f16111b1e6fbe9054049",
+    "url": "/goit-react-hw-02-feedback/static/css/main.51298aa0.chunk.css"
+  },
+  {
+    "revision": "7777eca6a20bc559c61c",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4875d002.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4875d002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16111b1e6fbe9054049",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f0ad1032.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
